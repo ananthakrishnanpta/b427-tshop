471f065149d21923ed29a7ff2477401385e6f4be
ananthakrishnanpta <ananthakrishnanpta@gmail.com>
2026-04-03 17:54:42 +0530

    added authentication

--- Tshop/authentication/views.py
+++ Tshop/authentication/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
+from django.urls import reverse_lazy
 
-# Create your views here.
+from django.contrib.auth.models import User 
+
+from django.contrib.auth.views import LoginView
+
+from django.views.generic import CreateView
+
+from .forms import UserRegistrationForm, UserLoginForm
+
+class UserRegisterView(CreateView):
+    model = User 
+    form_class  = UserRegistrationForm
+    template_name = 'authentication/register.html'
+    success_url = '/'
+
+class UserLoginView(LoginView):
+    template_name = 'authentication/login.html'
+    authentication_form = UserLoginForm
