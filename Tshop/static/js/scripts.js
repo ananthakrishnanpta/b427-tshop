c3e639701789a1ff164ac2d29454083fb9570182
ananthakrishnanpta <ananthakrishnanpta@gmail.com>
2026-04-08 18:09:49 +0530

    added script for add to cart button, need to implement request portion

---
+++ Tshop/static/js/scripts.js
@@ -0,0 +1,15 @@
+const productSection = document.querySelector('#product-section');
+
+productSection.addEventListener('click', async function (event){
+    const btn = event.target;
+    if (btn.classList.contains('add-to-cart')){
+        let productId;
+
+
+        const product_card = btn.closest('.product-card');
+
+        productId = product_card.dataset.productId;
+        alert(productId);
+    }
+
+})
