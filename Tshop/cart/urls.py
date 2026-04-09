22976de18037d7dc3411bc6398bf236e79f0dcce
ananthakrishnanpta <ananthakrishnanpta@gmail.com>
2026-04-09 15:11:50 +0530

    working on cart operations in AJAX

--- Tshop/cart/urls.py
+++ Tshop/cart/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.CartView.as_view(), name = 'view_cart')   
-    # path('', views.view_cart, name = 'view_cart')
+    path('', views.CartView.as_view(), name = 'view_cart'),   
+    
+    path('add/', views.AddToCartView.as_view(), name='add_to_cart')
 ]
--- Tshop/static/js/scripts.js
+++ Tshop/static/js/scripts.js
@@ -4,8 +4,6 @@ productSection.addEventListener('click', async function (event){
     const btn = event.target;
     if (btn.classList.contains('add-to-cart')){
         let productId;
-
-
         const product_card = btn.closest('.product-card');
 
         productId = product_card.dataset.productId;
